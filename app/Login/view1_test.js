b8f088aeda716b29d130c78aa3124243de0ba2b6
Tenali <nehemiahtenali@gmail.com>
2016-07-13 15:19:44 -0400

    I have renamed the file names view1.html to login.html view1.js to login.js

--- app/Login/view1_test.js
+++ app/Login/view1_test.js
@@ -1,10 +1,10 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('myApp.Login module', function() {
 
   beforeEach(module('myApp.view1'));
 
-  describe('view1 controller', function(){
+  describe('Login controller', function(){
 
     it('should ....', inject(function($controller) {
       //spec body
