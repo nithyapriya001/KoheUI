8446357909d71f236b023b8c6d6259d346d87dc9
MounikaReddy3 <mounikarani.rgukt@gmail.com>
2016-07-22 14:59:18 -0400

    Add files via upload

---
+++ kohe/src/webService/CreateuserService.java
@@ -0,0 +1,33 @@
+package webService;
+import java.util.ArrayList;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import com.google.gson.Gson;
+
+import dto.Login;
+import model.AccessManager;
+
+@Path("/timeportal")
+
+
+public class CreateuserService {
+	@GET
+	@Path("/createuser")
+	@Produces("application/json")
+	public String login() {
+		String login = null;
+		ArrayList<Login> loginList = new ArrayList<Login>();
+		try {
+			loginList = new AccessManager().getLoginpage();
+			Gson gson = new Gson();
+			login = gson.toJson(loginList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return login;
+	}
+}
+
